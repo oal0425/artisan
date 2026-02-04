d0b8161467b1610f8f3a37182f3acdaff346d744
Oscar <oal0425@gmail.com>
2026-02-04 00:03:06 -0300

    artisans products working

--- src/controllers/artisan.controller.js
+++ src/controllers/artisan.controller.js
@@ -1,21 +1,29 @@
-import Product from "../database/models/product.model.js";
-import {getAll, getById} from "../services/artisan.service.js";
+import Artisan from "../database/models/artisan.model.js";
+import { getAll, getById } from "../services/artisan.service.js";
 
-export const getAllArtisans = async (req, res) =>{
-    const artisans = await getAll();
-    res.render('artisans', {title:"Nuestros Artesanos", artisans});
+export const getAllArtisans = async (req, res) => {
+  const artisans = await getAll();
+  res.render("artisans", { title: "Nuestros Artesanos", artisans });
 };
 
-export const getArtisanById = async (req, res) =>{
-    const artisan = await getById(req.params.id);
-    res.render('artisanDetails', {title:artisan.name, artisan});
-}
+export const getArtisanById = async (req, res) => {
+  const artisan = await getById(req.params.id);
+  res.render("artisanDetails", { title: artisan.name, artisan });
+};
 
 export const getArtisanByParamsId = async (req, res) => {
-    const artisan = await getById(req.params.id);
-    if (!artisan){
-        return res.status(404).render("404", {title:"404"}); 
-    }
-    const products = await Product.find({ artisan: artisan._id}).populate("artisan").lean();
-    res.render("artisanDetails", {layout: "artisanDashboard", title: artisan.name, artisan, products});
-}
+  const artisan = await getById(req.params.id);
+  if (!artisan) {
+    return res.status(404).render("404", { title: "404" });
+  }
+  const artisanWithProducts = await Artisan.findById(artisan._id)
+    .populate("products")
+    .lean();
+
+  res.render("artisanDetails", {
+    layout: "artisanDashboard",
+    title: artisan.name,
+    artisan: artisanWithProducts,
+    products: artisanWithProducts.products,
+  });
+};
